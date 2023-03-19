1e3e7fcbb14b21b89e82ed7cb7699e4c1961ff52
Andrew Rubenstein <andjruben@gmail.com>
2023-03-18 22:44:57 -0400

    fix: move models to /models.py

--- src/dipdup_indexer/models.py
+++ src/dipdup_indexer/models.py
@@ -2,6 +2,16 @@ from tortoise import fields
 from dipdup.models import Model
 
 
-class User(Model):
-    address = fields.CharField(36, pk=True)
-    balance = fields.IntField()
+class Bet(Model):
+    user_address = fields.CharField(36, pk=True)
+    fa2_address = fields.CharField(36, index=True)
+    fa2_id = fields.CharField(4)
+    game_id = fields.IntField()
+    amount = fields.IntField()
+
+
+class Investment(Model):
+    user_address = fields.CharField(36, pk=True)
+    fa2_address = fields.CharField(36, index=True)
+    fa2_id = fields.IntField()
+    amount = fields.IntField()
--- src/dipdup_indexer/models/house.py
+++
@@ -1,9 +0,0 @@
-from tortoise import fields
-from dipdup.models import Model
-
-
-class Investment(Model):
-    user_address = fields.CharField(36, pk=True)
-    fa2_address = fields.CharField(36, index=True)
-    fa2_id = fields.IntField()
-    amount = fields.IntField()
