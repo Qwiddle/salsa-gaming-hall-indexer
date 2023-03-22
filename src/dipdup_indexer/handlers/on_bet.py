c1928b2c512c4dad938ad1d0b30ec7933ec9f745
Andrew Rubenstein <andjruben@gmail.com>
2023-03-22 00:11:46 -0400

    feat: index bet, bet resolve operations
    - updates matching bet row in bet table with the payout and status of the bet (true/false if user won)

--- src/dipdup_indexer/handlers/on_bet.py
+++ src/dipdup_indexer/handlers/on_bet.py
@@ -1,3 +1,5 @@
+import dipdup_indexer.models as models
+
 from dipdup.context import HandlerContext
 from dipdup.models import Transaction
 from dipdup_indexer.types.gaming_hall.parameter.make_bet import MakeBetParameter
@@ -8,4 +10,22 @@ async def on_bet(
     ctx: HandlerContext,
     make_bet: Transaction[MakeBetParameter, GamingHallStorage],
 ) -> None:
-    ...
+    try:
+        user_address = make_bet.data.sender_address
+        operation = make_bet.data.hash
+        fa2_address = make_bet.parameter.game_currency.fa2_address
+        fa2_id = make_bet.parameter.game_currency.token_id
+        amount = int(make_bet.parameter.bet)
+        game_id = int(next(iter(make_bet.storage.game_info)))
+
+        await models.Bet.create(
+            user_address=user_address,
+            operation=operation,
+            fa2_address=fa2_address,
+            fa2_id=fa2_id,
+            amount=amount,
+            game_id=game_id
+        )
+    except Exception as e:
+        print("Error in on_bet:")
+        print(e)
--- src/dipdup_indexer/handlers/on_bet_resolve.py
+++ src/dipdup_indexer/handlers/on_bet_resolve.py
@@ -1,3 +1,5 @@
+import dipdup_indexer.models as models
+
 from dipdup.context import HandlerContext
 from dipdup.models import Transaction
 from dipdup_indexer.types.gaming_hall.parameter.resolve_bet import ResolveBetParameter
@@ -8,4 +10,22 @@ async def on_bet_resolve(
     ctx: HandlerContext,
     resolve_bet: Transaction[ResolveBetParameter, GamingHallStorage],
 ) -> None:
-    ...
+    try:
+        game_id = resolve_bet.parameter.game_id
+        game_info = resolve_bet.storage.game_info[game_id]
+        payout = int(game_info.payout)
+        winner = game_info.winner
+
+        bet = await models.Bet.get(
+            game_id=game_id
+        )
+
+        if bet == None:
+            print("bet not found")
+        else:
+            bet.payout = payout
+            bet.winner = winner
+            await bet.save()
+    except Exception as e:
+        print("Error in on_bet_resolve:")
+        print(e)
