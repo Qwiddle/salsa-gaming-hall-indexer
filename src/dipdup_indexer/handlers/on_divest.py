b009abc40fa231531202d9ea659d814f60bdeab1
Andrew Rubenstein <andjruben@gmail.com>
2023-03-21 23:09:42 -0400

    feat: index stake, unstake operations

--- src/dipdup_indexer/handlers/on_divest.py
+++ src/dipdup_indexer/handlers/on_divest.py
@@ -1,3 +1,5 @@
+import dipdup_indexer.models as models
+
 from dipdup.context import HandlerContext
 from dipdup.models import Transaction
 from dipdup_indexer.types.gaming_hall.parameter.divest_shares import DivestSharesParameter
@@ -8,4 +10,20 @@ async def on_divest(
     ctx: HandlerContext,
     divest_shares: Transaction[DivestSharesParameter, GamingHallStorage],
 ) -> None:
-    ...
+    try:
+        user_address = divest_shares.data.sender_address
+        fa2_address = divest_shares.parameter.bankroll_currency.fa2_address
+        fa2_id = divest_shares.parameter.bankroll_currency.token_id
+        amount = int(divest_shares.parameter.amt)
+
+        await models.Investment.create(
+            user_address=user_address,
+            fa2_address=fa2_address,
+            fa2_id=fa2_id,
+            amount=amount,
+            type="unstake"
+        )
+
+    except Exception as e:
+        print("Error in on_divest_shares:")
+        print(e)
--- src/dipdup_indexer/handlers/on_invest.py
+++ src/dipdup_indexer/handlers/on_invest.py
@@ -1,3 +1,5 @@
+import dipdup_indexer.models as models
+
 from dipdup.context import HandlerContext
 from dipdup.models import Transaction
 from dipdup_indexer.types.gaming_hall.parameter.invest import InvestParameter
@@ -8,4 +10,20 @@ async def on_invest(
     ctx: HandlerContext,
     invest: Transaction[InvestParameter, GamingHallStorage],
 ) -> None:
-    ...
+    try:
+        user_address = invest.data.sender_address
+        fa2_address = invest.parameter.bankroll_currency.fa2_address
+        fa2_id = invest.parameter.bankroll_currency.token_id
+        amount = int(invest.parameter.amt)
+
+        await models.Investment.create(
+            user_address=user_address,
+            fa2_address=fa2_address,
+            fa2_id=fa2_id,
+            amount=amount,
+            type="stake"
+        )
+
+    except Exception as e:
+        print("Error in on_invest:")
+        print(e)
